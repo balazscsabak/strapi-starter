888322835b64ff7855fd9af0868d114da7b07b5e
balazscsabak <balazscsabak93@gmail.com>
2023-03-19 21:44:29 +0100

    add plugins

---
+++ config/plugins.ts
@@ -0,0 +1,17 @@
+export default (env) => ({
+  menus: {
+    config: {
+      maxDepth: 2,
+    },
+  },
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook: process.env.VERCEL_DEPLOY_HOOK,
+      apiToken: process.env.VERCEL_DEPLOY_API_TOKEN,
+      appFilter: process.env.VERCEL_DEPLOY_APP_FILTER,
+      teamFilter: process.env.VERCEL_DEPLOY_TEAM_FILTER,
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
+  },
+});
